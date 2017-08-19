887a032ad59c9299380ef156f2fb81a3d2d0948e
GiridharSamanth <samanth.giridhar@gmail.com>
2017-08-19 16:23:51 +0530

    Add 'Follow' APIs

--- src/models/follow.js
+++ src/models/follow.js
@@ -1,3 +1,91 @@
+// Initialize follow model
+function Follow(mysql) {
+    this.db = mysql;
+}
+
+//Function to add follow
+Follow.prototype.add = function (follow) {
+	
+	console.log("Model: Adding follow to database");
+	var that = this;
+    return new Promise(function (resolve, reject) {
+    	that.db.execute(
+    			'INSERT INTO FOLLOW (REFERENCE_TYPE,REFERENCE_ID,USER_ID,STATE) VALUES (?,?,?,?)  ON DUPLICATE KEY UPDATE STATE = ?',
+    			[follow.referenceType,follow.referenceId,follow.userId,follow.state,follow.state],
+    			function(err, result) {
+    				if (err) {
+    	                return reject(err);
+    	            }
+    				resolve(result);
+    			}
+    	);
+    });	
+};
+
+
+//Function to return list of follows by reference
+Follow.prototype.getByReference = function (referenceId) {
+	
+	console.log("Model: Getting list of follows from database by reference id");
+	var that = this;
+	return new Promise(function (resolve, reject) {
+		that.db.query(
+				'SELECT * FROM FOLLOW WHERE REFERENCE_ID = ? LIMIT 0,5',
+				[referenceId],	
+				function (err, result, fields) {
+					if (err) {
+						return reject(err);
+					}
+					resolve(result);
+				}
+		);
+	});
+};
+
+
+//Function to return list of follows by userId
+Follow.prototype.getByUser = function (userId) {
+	
+	console.log("Model: Getting list of follows from database by user id "+userId);
+	var that = this;
+	return new Promise(function (resolve, reject) {
+		that.db.query(
+				'SELECT * FROM FOLLOW WHERE USER_ID = ? LIMIT 0,5',
+				[userId],	
+				function (err, result, fields) {
+					if (err) {
+						return reject(err);
+					}
+					resolve(result);
+				}
+		);
+	});
+};
+
+
+//Function to return list of follows by reference and user
+Follow.prototype.getByRefernceAndUser = function (referenceId,userId) {
+	
+	console.log("Model: Getting follows from database by reference id and user id");
+	var that = this;
+	return new Promise(function (resolve, reject) {
+		that.db.query(
+				'SELECT * FROM FOLLOW WHERE REFERENCE_ID = ? AND USER_ID = ?',
+				[referenceId, userId],	
+				function (err, result, fields) {
+					if (err) {
+						return reject(err);
+					}
+					resolve(result);
+				}
+		);
+	});
+};
+
+
+
+
+/*
 var dbUtility = require( '../../lib/DbUtility.js' );
 
 var UserAuthorSchema = {
@@ -57,5 +145,6 @@ Follow.prototype.insert = function () {
 	  dbUtility.insert(follows[i]);
 	}
 };
+*/
 
 module.exports = Follow;
